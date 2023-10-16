674794553f6499369b7b709cdf39dc66e9d1daf0
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2023-10-17 02:19:28 +0900

    [Ed[t]패딩 조절

--- src/components/MainPage/layout/top.layout.jsx
+++ src/components/MainPage/layout/top.layout.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'; // useEffect 추가
+import React, { useEffect } from 'react';
 import NewsBox from "../news-box";
 import { useHistory } from 'react-router-dom';
 import styles from '../styles/main-page.module.css';
@@ -21,13 +21,17 @@ const TopLayout = () => {
 
   const getNews = async (query) => {
     try {
-      const response = await axios.get(`https://e715-123-254-143-22.ngrok-free.app/new?query=${query}`);
-      const news = response.data;
-      console.log(news);
+      const response = await axios.get(`https://13fe-123-254-143-22.ngrok-free.app/news?query=${query}`, {
+        headers: {
+          "ngrok-skip-browser-warning": "any"
+        }
+      });
+      console.log("API 요청 성공:", response);
+      // 요청이 성공적으로 완료될 경우, response에서 데이터를 처리할 수 있음
     } catch (error) {
       console.error("API 요청 오류:", error);
     }
-  } 
+  }
 
   useEffect(() => {
     const query = "주식"; // 뉴스 쿼리를 설정
@@ -38,7 +42,7 @@ const TopLayout = () => {
     <div className={styles.container}>
       <div className={styles.child_container}>
         <section className={styles.row_container}>
-          <TodayQuizBox stockQuiz={StockQuiz}/>
+          <TodayQuizBox stockQuiz={StockQuiz} />
           <NewsBox />
           <NewsBox />
           <NewsBox />
