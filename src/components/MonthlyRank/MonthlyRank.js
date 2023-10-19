05582232da851f451b217ce15eec12596d6960da
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2023-10-19 18:52:12 +0900

    [Add]월별 랭킹 더미데이터 추가

--- src/components/MonthlyRank/MonthlyRank.js
+++ src/components/MonthlyRank/MonthlyRank.js
@@ -5,14 +5,42 @@ import silverMedal from "../../assets/images/medal/silver.png";
 import bronzeMedal from "../../assets/images/medal/dong.png";
 
 function MonthlyRank({ topNGroups }) {
-  const processedData = processGroupData(topNGroups);
+  let processedData;
+  const dummyData = [
+    {
+      id: 0,
+      alt: "1",
+      groupName: "신투깐부",
+      groupRate: "87%",
+      src: getMedalImage(0),
+    },
+    {
+      id: 1,
+      alt: "2",
+      groupName: "주식킹",
+      groupRate: "98%",
+      src: getMedalImage(1),
+    },
+    {
+      id: 2,
+      alt: "3",
+      groupName: "우리가 최고",
+      groupRate: "105%",
+      src: getMedalImage(2),
+    },
+  ];
+  if (topNGroups.length === 0) {
+    processedData = dummyData;
+  } else {
+    processedData = processGroupData(topNGroups);
+  }
 
   function processGroupData(data) {
     // data 배열을 받아와서 가공
     return data.map((item, index) => ({
       id: index + 1,
-      alt: `그룹 이미지 ${index + 1}`,
-      src: getMedalImage(index),
+      alt: (index + 1).toString(),
+      src: getMedalImage(index + 1), // 수정: getMedalImage(id + 1) 대신 getMedalImage(index + 1)
       groupName: item.name,
       groupRate: `${item.profitRate}%`,
     }));
